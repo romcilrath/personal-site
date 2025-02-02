5563b2277622dd4cada7233de8c2381f6a5b3956
Robert McIlrath <romcilrath@gmail.com>
2025-02-02 18:36:28 -0500

    Navbar base added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Experience from './pages/Experience'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />=
         <Routes>
           <Route path="" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
       </Router>
-      <p>Hello world!</p>
     </div>
   );
 }
