baec2e29670f1bb260f0e250de4e1bd7ba1a34e8
Robert McIlrath <romcilrath@gmail.com>
2025-03-06 23:49:03 -0500

    Added headshot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import headshot from '../images/Me.png'; // Import the headshot image
 import "../styles/Home.css"; // Ensure the updated CSS file is imported
 
 export class Home extends Component {
@@ -9,6 +10,7 @@ export class Home extends Component {
     return (
       <div className="home">
         <div className="about">
+          <img className="headshot" src={headshot} alt="Me" /> {/* Reference the imported image */}
           <h2>Hi, I'm Robert McIlrath</h2>
           <div className="prompt">
             <p>
